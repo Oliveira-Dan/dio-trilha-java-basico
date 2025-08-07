b0028da5d5600063aeef3e08c664dfd5f54a0111
Oliveira-Dan <oliveira.busi.ss@gmail.com>
2025-08-07 17:21:05 -0300

    adição do codigo do aplicativo ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hora começar os trabalhos!");
+        Scanner scanner = new Scanner(System.in);
+        var nomeBanco = "DioBank";
+        var saldo = 237.48;
+        System.out.printf("Olá. Bem vindo ao banco %s! Crie sua conta agora. \n", nomeBanco);
+        System.out.println("Insira seus dados abaixo.");
+        System.out.print("Nome: ");
+        var nome = scanner.next();
+        System.out.print("Agência: ");
+        var agencia = scanner.next();
+        System.out.print("Conta: ");
+        var conta = scanner.nextInt();
+        System.out.println();
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco. Lembresse, sua agência é %s, conta %s, e como bônus de cadastro você tem o saldo de R$ %s. \n", nome, agencia, conta, saldo);
     }
 }
